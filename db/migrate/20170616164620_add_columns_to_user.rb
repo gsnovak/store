f22abe59a3692549c568028ca0e3967ce95828c2
gsnovak <gnovak.sh@gmail.com>
2017-06-16 11:50:20 -0500

    playing around w/ gitHub desktop

---
+++ db/migrate/20170616164620_add_columns_to_user.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.1]
+  def change
+    change_table(:users) do |t|
+    t.column :first_name, :integer
+    t.column :last_name, :integer
+    t.column :user_name, :integer, unique: true
+    # Other column alterations here
+    end
+  end
+end
