ea33e846cd5c4518c0ee4140fe344ef21053d26a
gsnovak <gnovak.sh@gmail.com>
2017-06-16 15:21:23 -0500

    order model testing for transitions

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,13 +1,33 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  test 'test valid order' do
+  test 'transition from cart to placed' do
     order = create(:order)
-    assert order.valid?
+    product = create(:product)
+
+    order_item = OrderItem.create(order_id: order.id, source_type: Product.name, source_id: product.id, quantity: 10)
+    order.add_order_item(order_item)
+
+    assert order.make_placed
   end
 
-  test 'order invalid without state' do
-    #todo test valid state
+  test 'cannot transition from cart to canceled' do
+    order = create(:order)
+    product = create(:product)
+
+    order_item = OrderItem.create(order_id: order.id, source_type: Product.name, source_id: product.id, quantity: 10)
+    order.add_order_item(order_item)
+
+    assert_not order.make_canceled
   end
 
+  test 'cannot place an empty order' do
+    order = create(:order)
+    product = create(:product)
+
+    order_item = OrderItem.create(order_id: order.id, source_type: Product.name, source_id: product.id, quantity: 0)
+    order.add_order_item(order_item)
+
+    assert_not order.make_placed
+  end
 end
