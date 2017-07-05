994b6e2dd195e156bae86277ea50c8a1705ba776
gsnovak <gnovak.sh@gmail.com>
2017-07-05 14:37:04 -0500

    postgres

--- db/migrate/20170607144212_remove_column_from_credit_card.rb
+++ db/migrate/20170607144212_remove_column_from_credit_card.rb
@@ -1,5 +1,5 @@
 class RemoveColumnFromCreditCard < ActiveRecord::Migration[5.1]
   def change
-    remove_column :credit_cards, :experation_date, :datetime
+    remove_column :credit_cards, :expiration_date, :datetime
   end
 end
