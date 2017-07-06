3126700ddc390d12e1a8e4fb456439cf027e75ad
gsnovak <gnovak.sh@gmail.com>
2017-07-06 09:53:55 -0500

    fix

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :image_url, :quantity, :picture_url
+  attributes :id, :name, :price, :quantity, :picture_url
 
   def quantity
     return 0
