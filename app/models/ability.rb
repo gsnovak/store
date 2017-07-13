d65375673afe122d81db3b15c45ea10d13893d28
graham <novak.graham@yahoo.com>
2017-07-13 16:15:27 -0500

    state shouldn't be crudable

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,6 @@ class Ability
         can :manage, Order, user_id: user.id
         can :crud, CreditCard, user_id: user.id
         can :crud, Address, user_id: user.id
-        can :crud, State
       end
   end
 end
