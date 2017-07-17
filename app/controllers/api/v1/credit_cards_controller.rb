acad51da48f9d1afb4ced8baf0883b820c256951
gsnovak <gnovak.sh@gmail.com>
2017-07-17 14:18:22 -0500

    fix - cc wasn't updating

--- app/controllers/api/v1/credit_cards_controller.rb
+++ app/controllers/api/v1/credit_cards_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::CreditCardsController < Api::V1::BaseController
 
-  wrap_parameters :credit_card, include: CreditCard.column_names + [:number]
+  wrap_parameters :credit_card, include: CreditCard.column_names + [:number, :user_id]
 
   def update
-   if @source_model.update(credit_card_params) && @source_model.errors.empty?
-      render status: 200, json: @source_model
-    else
+   unless @source_model.update(credit_card_params) && @source_model.errors.empty?
       render status: 400, json: @source_model.errors.full_messages
     end
   end
