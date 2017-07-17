df126a24d4674b642e7646c8081f17bf711e7171
gsnovak <gnovak.sh@gmail.com>
2017-07-17 14:01:51 -0500

    fix - cc controller broke cc display errors

--- app/controllers/api/v1/credit_cards_controller.rb
+++ app/controllers/api/v1/credit_cards_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::CreditCardsController < Api::V1::BaseController
   wrap_parameters :credit_card, include: CreditCard.column_names + [:number]
 
   def update
-    @source_model.update(credit_card_params)
+   if @source_model.update(credit_card_params) && @source_model.errors.empty?
+      render status: 200, json: @source_model
+    else
+      render status: 400, json: @source_model.errors.full_messages
+    end
   end
 
   private
