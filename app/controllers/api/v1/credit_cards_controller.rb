5eb906179bc8fd1aad31e5d8ed08219fb925f7cc
gsnovak <gnovak.sh@gmail.com>
2017-07-19 10:51:11 -0500

    serializer issue

--- app/controllers/api/v1/credit_cards_controller.rb
+++ app/controllers/api/v1/credit_cards_controller.rb
@@ -2,6 +2,13 @@ class Api::V1::CreditCardsController < Api::V1::BaseController
 
   wrap_parameters :credit_card, include: CreditCard.column_names + [:number, :user_id]
 
+  def create
+    @source_model = CreditCard.new(credit_card_params)
+    unless @source_model.save(credit_card_params) && @source_model.errors.empty?
+      render status: 400, json: @source_model.errors.full_messages
+    end
+  end
+
   def update
    unless @source_model.update(credit_card_params) && @source_model.errors.empty?
       render status: 400, json: @source_model.errors.full_messages
