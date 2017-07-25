b888890f6f9d1247e2f4fdce4b4f2af253df36cb
gsnovak <gnovak.sh@gmail.com>
2017-07-25 12:43:55 -0500

    for heroku

--- db/migrate/20170725173803_change_columns_in_user.rb
+++ db/migrate/20170725173803_change_columns_in_user.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsInUser < ActiveRecord::Migration[5.1]
   def change
-    change_column :user, :first_name, :string
-    change_column :user, :last_name, :string
+    change_column :users, :first_name, :string
+    change_column :users, :last_name, :string
   end
 end
