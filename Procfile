538c00f892c7e0ef141e06560f15c7204278a8e5
gsnovak <gnovak.sh@gmail.com>
2017-07-28 14:39:17 -0500

    heraka

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-mailers: bundle exec sidekiq -q mailers
 default: bundle exec sidekiq -q default
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module RailsStorev1
     config.load_defaults 5.1
     config.cache_store = :redis_store, "#{ENV["REDIS_URL"]}/0/cache", { expires_in: 90.minutes }
     config.active_job.queue_adapter = :sidekiq
+    config.action_mailer.deliver_later_queue_name = 'default'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
