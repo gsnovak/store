c62658803a6696e5c228d9a19a2f7cb10ce8b3ab
gsnovak <gnovak.sh@gmail.com>
2017-07-28 14:36:18 -0500

    hero-ku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails_storev1_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.deliver_later_queue_name = 'default'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
