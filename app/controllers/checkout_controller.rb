e4cd5123141fb421bc66abf80b07e598865ef99e
gsnovak <gnovak.sh@gmail.com>
2017-07-31 09:03:20 -0500

    added new fields to thank you page

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,5 @@
 class  CheckoutController < ApplicationController
+  before_action :is_signed_in?
 
   def index
       redirect_to new_user_session_path if current_user.nil?
@@ -11,5 +12,11 @@ class  CheckoutController < ApplicationController
     @address = current_user.address || Address.new(user_id: current_user.id)
     @credit_card = current_user.credit_card || CreditCard.new(user_id: current_user.id)
     render 'checkout/thank_you'
+  end
+
+  private
+
+  def is_signed_in?
+    redirect_to new_user_session_path if !user_signed_in?
   end
 end
